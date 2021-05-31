2877610a597752da6be19893c17078b75fdb69b0
Matas Motuzas <mata0018@elev.tec.dk>
2021-05-31 12:14:42 +0200

    Helt basic mapper

---
+++ TecH3Projekt.API/Domain/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TecH3Projekt.API.Domain
+{
+    public class Product
+    {
+    }
+}
