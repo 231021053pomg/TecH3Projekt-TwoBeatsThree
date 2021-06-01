53ea6f4b9a6f553e413b3801f11b11f5b8369584
Matas Motuzas <mata0018@elev.tec.dk>
2021-06-01 11:33:23 +0200

    Added some Domains

---
+++ TecH3Projekt.API/Domain/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TecH3Projekt.API.Domain
+{
+    public class Order : BaseModel
+    {
+        [ForeignKey("LogIn.Id")]
+        public int LogInId { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
