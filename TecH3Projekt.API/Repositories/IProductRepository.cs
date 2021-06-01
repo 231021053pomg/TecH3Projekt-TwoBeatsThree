9f92775dbed2b4737789383cc72dd20af272280a
Francis John Owen <fj.owen@live.dk>
2021-06-01 14:03:35 +0200

    Product interface/Repo

---
+++ TecH3Projekt.API/Repositories/IProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TecH3Projekt.API.Domain;
+
+namespace TecH3Projekt.API.Repositories
+{
+    public interface IProductRepository
+    {
+        // Get all Product objects
+        Task<List<Product>> GetAll();
+
+        //Get Product object by id.
+        Task<Product> GetById(int id);
+
+        //Create Product
+        Task<Product> Create(Product product);
+
+        //Update Product
+        Task<Product> Update(int id, Product product);
+
+        //Delete Product
+        Task<Product> Delete(int id);
+    }
+}
