5172310d3ca39f5fd8ada99fbe946dc61fdf92ce
Matas Motuzas <mata0018@elev.tec.dk>
2021-06-01 10:43:14 +0200

    Basemodel

---
+++ TecH3Projekt.API/Domain/BaseModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TecH3Projekt.API.Domain
+{
+    public class BaseModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [JsonIgnore]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonIgnore]
+        public DateTime? UpdatedAt { get; set; }
+
+        [JsonIgnore]
+        public DateTime? DeletedAt { get; set; }
+    }
+}
