8e846558404902037296e33ecc5aa8b867e2b205
Francis John Owen <fj.owen@live.dk>
2021-06-01 12:13:02 +0200

    Domains: Picture, Property, Product_Property, Type.

--- TecH3Projekt.API/Domain/Product_Property.cs
+++ TecH3Projekt.API/Domain/Product_Property.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TecH3Projekt.API.Domain
 {
-    public class Picture
+    public class Product_Property
     {
-        public string src { get; set; }
-
         [ForeignKey("Product.Id")]
         public int ProductId { get; set; }
+
+        [ForeignKey("Property.Id")]
+        public int PropertyId { get; set; }
     }
 }
--- TecH3Projekt.API/Domain/Property.cs
+++ TecH3Projekt.API/Domain/Property.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TecH3Projekt.API.Domain
 {
-    public class Type : BaseModel
+    public class Property
     {
-       [Required]
         public string Name { get; set; }
     }
 }
