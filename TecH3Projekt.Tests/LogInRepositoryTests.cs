1d6476883ff648df6173d1f5ba6f7b0204e43466
Matas Motuzas <mata0018@elev.tec.dk>
2021-06-03 11:06:57 +0200

    added LogInRepositoryTests

---
+++ TecH3Projekt.Tests/LogInRepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TecH3Projekt.API.Database;
+using TecH3Projekt.API.Repositories;
+using Xunit;
+
+namespace TecH3Projekt.Tests
+{
+    public class LogInRepositoryTests
+    {
+        private DbContextOptions<TecH3ProjectDbContext> _options;
+        private readonly TecH3ProjectDbContext _context;
+
+
+        [Fact]
+        public async Task GetAllLogIns_ShouldReturnAllLogIns_WhenLogIndsExists()
+        {
+            // Arrange
+            LogInRepository logInRepository = new LogInRepository(_context);
+
+            // Act
+            var logIn = await logInRepository.GetAll();
+
+            // Assert
+            Assert.Equal(3, logIn.Count);
+        }
+    }
+}
