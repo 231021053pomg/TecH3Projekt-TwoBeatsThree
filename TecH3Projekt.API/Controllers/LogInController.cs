74aca350b2320d255c95f1263108bb0f9883939c
Francis John Owen <fj.owen@live.dk>
2021-06-08 14:21:31 +0200

    Controllers

---
+++ TecH3Projekt.API/Controllers/LogInController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TecH3Projekt.API.Domain;//
+using TecH3Projekt.API.Services;//
+
+namespace TecH3Projekt.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogInController : ControllerBase
+    {
+        //inject service into controller.
+        private readonly ILogInService _loginService;
+
+        //ctor gets IUserService, which allows for use of classes with IUserService implemented.
+        public LogInController(ILogInService loginService)
+        {
+            _loginService = loginService;// connect functions.
+        }
+
+        //EXAMPLE: https://localhost:5001/api/login
+        [HttpGet]//endpoint
+        public async Task<IActionResult> GetAll()
+        {
+            var logins = await _loginService.GetAllLogIns();
+            return Ok(logins);
+        }
+        //EXAMPLE: https://localhost:5001/api/login
+        [HttpPost]
+        public async Task<IActionResult> Create(LogIn login)
+        {
+            var newLogIn = await _loginService.Create(login);
+            return Ok(newLogIn);
+        }
+    }
+}
